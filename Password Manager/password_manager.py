17408c45d69cf932560ab42c890c19385e966def
Adam Burakowski <125091826+AdamBurakowski@users.noreply.github.com>
2023-03-14 23:48:24 +0100

    Update password_manager.py
    Rewrote comments to be more understandable

--- Password Manager/password_manager.py
+++ Password Manager/password_manager.py
@@ -2,11 +2,11 @@ import random as r
 import os
 from cryptography.fernet import Fernet
 
-# path to directory with passwords
+# This creates a string variable that stores the name of a directory where passwords will be stored.
 passwords_folder = "passwords"
 
 
-# function responsible for loading tey used for encryption and decryption of data
+# This is a function that reads in a key from a file named "key.key" and returns it.
 def load_key():
 	file = open("key.key", "rb")
 	key = file.read()
@@ -14,7 +14,8 @@ def load_key():
 	return key
 
 
-# function responsible for creating new data and encrypting it
+# This is a function that prompts the user to enter information about a new password (platform, username, desired length, etc.)
+# and generates a random password based on that information.
 def create_password():
 	platform = input("Type name of the platform in which you are creating account: ")
 	username = input("Type your username on that platform: ")
@@ -28,7 +29,10 @@ def create_password():
 	if input("Do you want numbers in your password y/n? ") == "y":
 		types_of_characters.append("n")
 
-# generating password
+
+# This for loop is used to generate a random password.
+# It repeats num_of_characters times (which is set by the user) and randomly selects characters to add to the password
+# based on the types of characters specified by the user.
 	for times in range(num_of_characters):
 		character = str(types_of_characters[r.randint(0, len(types_of_characters) - 1)])
 		if character == "s_l":
@@ -37,15 +41,16 @@ def create_password():
 			password += english_alphabet[r.randint(0, len(english_alphabet) - 1)].upper()
 		else:
 			password += str(r.randint(0, 9))
-	if not os.path.exists(passwords_folder):  # creates passwords folder if not present
+	if not os.path.exists(passwords_folder):  # This creates the passwords directory if it doesn't already exist.
 		os.makedirs(passwords_folder)
 	os.chdir(passwords_folder)
+	# This creates a new file in the passwords directory with a unique name based on the number of files already in the directory.
+	# It then writes encrypted data to the file.
 	with open(f"password{len(os.listdir()) + 1}", "x") as f:
-		# writing encrypted data to the new file
 		f.write(f"{str(fernet.encrypt(platform.encode()).decode())}\n{fernet.encrypt(username.encode()).decode()}\n{str(fernet.encrypt(password.encode()).decode())}")
 
 
-# function responsible for decrypting and showing data from passwords directory
+# This is a function that reads in encrypted data from the password files, decrypts it, and displays the decrypted information to the user.
 def decrypt_password():
 	descriptions = {1: "platform: ", 2: "username: ", 3: "password: "}  # tells program what to write depending on a line in a file
 	files = os.listdir(passwords_folder)  # finds all files in a password directory
@@ -60,13 +65,15 @@ def decrypt_password():
 				line_count += 1
 			print("\n-------------------------------------------------------------------------------\n")
 
-
-master_password = input("Password: ") # master password is used as part of a key
-key = load_key() + master_password.encode()  # master password is combined with a key
+# This prompts the user to enter a master password, combines it with the key loaded from the "key.key" file,
+# and creates a new Fernet object for encrypting and decrypting data.
+master_password = input("Password: ")
+key = load_key() + master_password.encode()
 fernet = Fernet(key)
 
 
-# function responsible for choosing type of action
+# This is the main function that prompts the user to choose between creating a new password or decrypting an existing one,
+# and calls the appropriate function based on the user's choice.
 def main():
 	valid_option = False
 	while not valid_option:
@@ -86,5 +93,7 @@ def main():
 			print("Invalid command. Please try again.\n\n\n")
 
 
+# This checks whether the current script is being run as the main program (as opposed to being imported as a module by another program)
+# and calls the main() function if it is.
 if __name__ == "__main__":
 	main()
